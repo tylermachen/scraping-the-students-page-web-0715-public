471c27130e1c58a16525b5ea8a34009c5b4a5041
Tyler Machen <tylermachen@gmail.com>
2015-08-06 18:30:40 -0400

    add create_student_hash now that website works

--- lib/students.rb
+++ lib/students.rb
@@ -29,6 +29,38 @@ def create_student_hash
 end
 =end
 
+def create_student_hash
+  url = 'http://web0715.students.flatironschool.com/'
+  begin
+    html = open(url)
+    profile_data = Nokogiri::HTML(html)
+    students = {}
+
+    # iterate over all student bios and return student profiles
+    profile_data.css("div.big-comment h3 a").each do |student|
+      name = student.text
+      profile_url = student.values.first
+
+      student_html = open('http://web0715.students.flatironschool.com/' + profile_url)
+      student_profile_data = Nokogiri::HTML(student_html)
+
+      tagline = student_profile_data.css("div.textwidget h3").text
+      work = student_profile_data.css("div.services h4").text
+      education = student_profile_data.css("div.services ul li").text
+
+      students[name] = Student.new(name, profile_url, tagline, work, education)
+    end
+  rescue OpenURI::HTTPError => e
+    if e.message == '404 Not Found'
+      puts "EXPLODE!!!!"
+    end
+  end
+
+  students
+end
+
+completed_hash = create_student_hash
+
 # create student
 class Student
   @default = 'no info provided'
@@ -42,11 +74,12 @@ class Student
   end
 end
 
+=begin
 students = {}
 students["John Smith"] = Student.new('John Smith', nil,'Living the good life', 'Harvard', 'CEO')
 students["Seymore Butts"] = Student.new('Seymore Butts',nil, 'Living the best life!', 'MIT', 'Chicken Sexer')
 students["Bill Bonovan"] = Student.new('Bill Bonovan', nil, 'Living an even better life.', 'Yale', 'Jedi Master')
-
+=end
 ###### iterate over all student objects and add them to an array
 # steven = Student.new(name, tagline, bio)
 
@@ -120,5 +153,5 @@ class Directory
   end
 end
 
-directory = Directory.new(students)
+directory = Directory.new(completed_hash)
 directory
