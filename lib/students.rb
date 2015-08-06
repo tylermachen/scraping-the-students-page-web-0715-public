6778bf74c0cafa1d6a7f7faf7be23fdf881c08f0
Tyler Machen <tylermachen@gmail.com>
2015-08-06 16:31:20 -0400

    add file structure and add error handling file

--- lib/students.rb
+++ lib/students.rb
@@ -8,15 +8,19 @@ def create_student_hash
   profile_data = Nokogiri::HTML(html)
   students = {}
 
+  # iterate over all student bios and return student profiles
   profile_data.css("div.big-comment h3 a").each do |student|
    name = student.text
    profile_url = student.values.first
 
    student_html = open('http://web0715.students.flatironschool.com/' + profile_url)
    student_profile_data = Nokogiri::HTML(student_html)
-   tagline = student_profile_data.css("div.textwidget h3").text
 
-   students[name] = Student.new(name, profile_url, tagline)
+   tagline = student_profile_data.css("div.textwidget h3").text
+   work = student_profile_data.css("div.services h4").text
+   education = student_profile_data.css("div.services ul li").text
+
+   students[name] = Student.new(name, profile_url, tagline, work, education)
    binding.pry
   end
   students
@@ -24,11 +28,14 @@ end
 
 # create student
 class Student
-  attr_accessor :name, :profile_url, :tagline
-  def initialize(name, profile_url, tagline)
+  @default = 'no info provided'
+  attr_accessor :name, :profile_url, :tagline, :work, :education
+  def initialize(name, profile_url, tagline = @default, work = @default, education = @default)
     @name = name
     @profile_url = profile_url
     @tagline = tagline
+    @work = work
+    @education = education
   end
 end
 
---
+++ lib/students_with_exception_handling.rb
@@ -0,0 +1,55 @@
+=begin THIS ONE HANDLES 404 ERRORS
+def create_student_hash
+  url = 'http://web0715.students.flatironschool.com/'
+  begin
+    html = open(url)
+    profile_data = Nokogiri::HTML(html)
+    students = {}
+
+    # iterate over all student bios and return student profiles
+    profile_data.css("div.big-comment h3 a").each do |student|
+      name = student.text
+      profile_url = student.values.first
+
+      student_html = open('http://web0715.students.flatironschool.com/' + profile_url)
+      student_profile_data = Nokogiri::HTML(student_html)
+
+      tagline = student_profile_data.css("div.textwidget h3").text
+      work = student_profile_data.css("div.services h4").text
+      education = student_profile_data.css("div.services ul li").text
+
+      students[name] = Student.new(name, profile_url, tagline, work, education)
+      binding.pry
+    end
+  rescue OpenURI::HTTPError => e
+    if e.message == '404 Not Found'
+      puts "EXPLODE!!!!"
+    end
+  end
+end
+=end
+
+=begin ORIGINAL WORKING METHOD WHEN SITE WORKS
+def create_student_hash
+  html = open('http://web0715.students.flatironschool.com/')
+  profile_data = Nokogiri::HTML(html)
+  students = {}
+
+  # iterate over all student bios and return student profiles
+  profile_data.css("div.big-comment h3 a").each do |student|
+   name = student.text
+   profile_url = student.values.first
+
+   student_html = open('http://web0715.students.flatironschool.com/' + profile_url)
+   student_profile_data = Nokogiri::HTML(student_html)
+
+   tagline = student_profile_data.css("div.textwidget h3").text
+   work = student_profile_data.css("div.services h4").text
+   education = student_profile_data.css("div.services ul li").text
+
+   students[name] = Student.new(name, profile_url, tagline, work, education)
+   binding.pry
+  end
+  students
+end
+=end
